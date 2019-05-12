295b65ee13ae4dbcc2d1be35ecfc3191058262ea
k.tang@yale.edu <just.ktang@gmail.com>
2019-05-12 01:08:21 -0400

    enable routing to tournament results and scores

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,21 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :tournaments do
-    resources :events
+    member do
+      get 'results' => 'tournaments#results', as: 'results'
+    end
+
+    resources :events do
+      member do
+        get 'assign_scores' => 'events#assign_scores', as: 'assign_scores'
+      end
+    end
+
     resources :teams
     resources :admin_roles
     resources :event_supervisor_roles
+    resources :scores, only: %i(create update destroy)
   end
 
   resources :users
-end
+end   
