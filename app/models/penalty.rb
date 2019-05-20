34e75b927058d11d41c29430626cac12870aba87
Em Zhan <zqianem@gmail.com>
2019-05-20 10:53:55 -0400

    Validate greater than zero for penalty points

--- app/models/penalty.rb
+++ app/models/penalty.rb
@@ -2,5 +2,6 @@ class Penalty < ApplicationRecord
   belongs_to :team
 
   validates :team_id, presence: true
-  validates :points, presence: true
+  validates :points, presence: true,
+                     numericality: { greater_than: 0 }
 end
